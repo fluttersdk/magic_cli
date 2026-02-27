c59ea20db24bf11299f3254f61c71c4f46bd710b
Anilcan Cakir <anilcan.cakir@gmail.com>
2026-02-28 02:16:13 +0300

    feat(magic_cli): update barrel export

--- lib/magic_cli.dart
+++ lib/magic_cli.dart
@@ -3,11 +3,36 @@ export 'package:args/args.dart';
 
 // Console
 export 'src/console/command.dart';
+export 'src/console/generator_command.dart';
 export 'src/console/kernel.dart';
+export 'src/console/string_helper.dart';
 
-// Helpers (will be implemented in following phases)
+// Helpers
+export 'src/helpers/config_editor.dart';
 export 'src/helpers/console_style.dart';
 export 'src/helpers/file_helper.dart';
-export 'src/helpers/config_editor.dart';
+export 'src/helpers/html_editor.dart';
+export 'src/helpers/json_editor.dart';
+export 'src/helpers/platform_helper.dart';
+export 'src/helpers/xml_editor.dart';
 
 // Stubs
+export 'src/stubs/stubs.dart';
+
+// Commands
+export 'src/commands/install_command.dart';
+export 'src/commands/key_generate_command.dart';
+export 'src/commands/make_controller_command.dart';
+export 'src/commands/make_enum_command.dart';
+export 'src/commands/make_event_command.dart';
+export 'src/commands/make_factory_command.dart';
+export 'src/commands/make_lang_command.dart';
+export 'src/commands/make_listener_command.dart';
+export 'src/commands/make_middleware_command.dart';
+export 'src/commands/make_migration_command.dart';
+export 'src/commands/make_model_command.dart';
+export 'src/commands/make_policy_command.dart';
+export 'src/commands/make_provider_command.dart';
+export 'src/commands/make_request_command.dart';
+export 'src/commands/make_seeder_command.dart';
+export 'src/commands/make_view_command.dart';
